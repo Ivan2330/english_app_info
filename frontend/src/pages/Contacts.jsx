e138fc032fced8a476b9ade86fa67a1c55d772a8
Ivan2330 <162325881+Ivan2330@users.noreply.github.com>
2025-10-17 05:28:49 +0300

    //

--- frontend/src/pages/Contacts.jsx
+++ frontend/src/pages/Contacts.jsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
 import { createLead } from "../api/leads.js";
-import "./Contact.css";
+
 
 /* ===== Icons ===== */
 function IconUser(){
